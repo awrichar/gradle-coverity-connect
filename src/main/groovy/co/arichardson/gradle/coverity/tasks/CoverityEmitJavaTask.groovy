bb4020137547225429b83d3bee483ea4a70751d2
Andrew Richardson <awrichardson6@gmail.com>
2016-08-12 12:25:20 -0400

    Fix potential NPE in CoverityEmitJavaTask

--- src/main/groovy/co/arichardson/gradle/coverity/tasks/CoverityEmitJavaTask.groovy
+++ src/main/groovy/co/arichardson/gradle/coverity/tasks/CoverityEmitJavaTask.groovy
@@ -17,9 +17,15 @@ class CoverityEmitJavaTask extends AbstractCoverityIntermediatesTask {
         args '--dir', intermediatesDir.path
         args '--compiler-outputs', compileTask.destinationDir.path
         args '--classpath', compileTask.classpath.asPath
-        args '--bootclasspath', compileTask.options.bootClasspath
-        args '--encoding', compileTask.options.encoding
         args '--source', compileTask.sourceCompatibility
+
+        if (compileTask.options.bootClasspath) {
+            args '--bootclasspath', compileTask.options.bootClasspath
+        }
+        if (compileTask.options.encoding) {
+            args '--encoding', compileTask.options.encoding
+        }
+
         compileTask.source.files.each { args it.path }
     }
 }
